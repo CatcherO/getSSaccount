a4932da7e88593b67915807028644b001e2bf950
liu <liuhui2583@qq.com>
2018-06-04 14:24:06 +0800

    done

--- app.js
+++ app.js
@@ -2,38 +2,38 @@
 var cheerio = require('cheerio');
 var superagent = require('superagent');
 var fs = require('fs');
-var conf = require('./gui-config')
+var confs = [];
 
-
+const random = length => {
+    return Math.floor(length*Math.random())
+ }
  superagent.get('https://my.ishadowx.net/')
     .end(function (err, sres) {
         if (err) {
             return next(err);
         }
         var $ = cheerio.load(sres.text);
-        var config = [];
         $('.portfolio-items .portfolio-item .hover-text').each(function (idx, element) {
             var $element = $(element);
             const server = $element.find('h4:nth-child(1) span:nth-child(1)').text().trim();
             const server_port = $element.find('h4:nth-child(2) span:nth-child(1)').text().trim();
             const password = $element.find('h4:nth-child(3) span:nth-child(1)').text().trim();
             if(server && server_port && password) {
-                config.push({
+                confs.push({
                     server,
                     server_port,
+                    local_address: "127.0.0.1",
+                    local_port: 1080,
                     password,
+                    timeout: 300,
                     method: "aes-256-cfb",
-                    plugin: "",
-                    plugin_opts: "",
-                    plugin_args: "",
-                    remarks: "",
-                    timeout: 5
+                    fast_open: false
                 });
             }
         });
         
-        conf.configs = config
-        fs.writeFile('gui-config.json',JSON.stringify(conf, null,' '), function (err) {
+        
+        fs.writeFile('conf.json',JSON.stringify(confs[random(confs.length)], null,' '), function (err) {
             if (err) {
                 console.log(err);
             } else {
@@ -43,7 +43,6 @@ var conf = require('./gui-config')
       
     });
     
-
-
+    
 
     
